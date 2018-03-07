b53ac03e9e46eb0d9e9bc19cd94317626bef19b4
Brooke <bra65@uclive.ac.nz>
2018-03-07 19:29:03 +1300

    Added last of commands to validation check
    #story[50]

--- src/main/java/odms/commandlineview/Main.java
+++ src/main/java/odms/commandlineview/Main.java
@@ -42,22 +42,35 @@ public class Main {
                         attrArray.add(attr);
                     }
                     Donor newDonor = new Donor(attrArray);
+                    DonorDatabase.add(newDonor);
                     //store the donor.
                     break;
 
                 case 5:
-                    //search profiles (donor).
+                    //search profiles (donor > view).
                     System.out.println("Searching for profiles...");
                     //carry out method call in command.
                     break;
 
                 case 6:
+                    //search profiles (donor > date-created).
+                    System.out.println("Searching for profiles...");
+                    //carry out method call in command.
+                    break;
+
+                case 7:
+                    //search profiles (donor > donations).
+                    System.out.println("Searching for profiles...");
+                    //carry out method call in command.
+                    break;
+
+                case 8:
                     //search profiles.
                     System.out.println("Updating profiles...");
                     //carry out method call in command.
                     break;
 
-                case 7:
+                case 9:
                     System.out.println("Please enter a valid command.");
                     break;
             }
