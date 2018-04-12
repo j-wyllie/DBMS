ef2d33b344f432e1a65c45f8e56432a01cfe84ac
Jack Hay <jackhaaay@gmail.com>
2018-04-12 12:15:44 +1200

    Changed to snake case and added final to constants.

--- src/main/java/odms/controller/GuiMain.java
+++ src/main/java/odms/controller/GuiMain.java
@@ -17,11 +17,11 @@ import odms.user.UserType;
 
 public class GuiMain extends Application {
 
-    private static String DONORDATABASE = "example/example.json";
-    private static String USERDATABASE = "example/users.json";
+    private static final String DONOR_DATABASE = "example/example.json";
+    private static final String USER_DATABASE = "example/users.json";
 
-    private static DonorDatabase donorDb = DonorDataIO.loadData(DONORDATABASE);
-    private static UserDatabase userDb = new UserDataIO().loadData(USERDATABASE);
+    private static DonorDatabase donorDb = DonorDataIO.loadData(DONOR_DATABASE);
+    private static UserDatabase userDb = new UserDataIO().loadData(USER_DATABASE);
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -31,7 +31,7 @@ public class GuiMain extends Application {
             userDb = new UserDatabase();
             User user = new User(UserType.CLINICIAN, "Doc", "Christchurch");
             userDb.addClinician(user);
-            UserDataIO.saveUsers(userDb, USERDATABASE);
+            UserDataIO.saveUsers(userDb, USER_DATABASE);
         }
         Parent root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
         primaryStage.setScene(new Scene(root));
@@ -39,8 +39,8 @@ public class GuiMain extends Application {
 
         primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
             public void handle(WindowEvent we) {
-                DonorDataIO.saveDonors(donorDb, DONORDATABASE);
-                UserDataIO.saveUsers(userDb, USERDATABASE);
+                DonorDataIO.saveDonors(donorDb, DONOR_DATABASE);
+                UserDataIO.saveUsers(userDb, USER_DATABASE);
             }
         });
     }
