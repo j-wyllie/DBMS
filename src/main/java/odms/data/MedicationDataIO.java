5fb52ef967c3b1ffa35b8ff44a184a9175341cb1
Brooke <bra65@uclive.ac.nz>
2018-04-17 22:39:52 +1200

    Tests written for MedicationDataIO class
    #story[554]

--- src/main/java/odms/data/MedicationDataIO.java
+++ src/main/java/odms/data/MedicationDataIO.java
@@ -1,56 +1,83 @@
 package odms.data;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import netscape.javascript.JSObject;
 
 public class MedicationDataIO {
 
-    public static List<String> GetSuggestionList(String substring) {
-        List<String> suggestionList = Arrays.asList();
-        try {
-            String urlString = String.format("mapi-us.iterar.co/api/autocomplete?query=%s", substring);
+    /**
+     * Gets a list of suggestions for the clinician user based on a string or substring they
+     * have entered into the field.
+     * @param substring represents the value to send in the HTTP GET request to the API.
+     * @throws IOException
+     */
+    public static ArrayList<String> GetSuggestionList(String substring) throws IOException{
+        ArrayList<String> suggestionList = new ArrayList<>();
+
+        if (!(substring == null || substring == "")) {
+            String urlString = String
+                    .format("http://mapi-us.iterar.co/api/autocomplete?query=%s", substring);
             URL url = new URL(urlString);
+
+            //Creating the connection to the API server.
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
             con.setRequestProperty("Content-Type", "application/json");
             con.setConnectTimeout(5000);
             con.setReadTimeout(5000);
 
-            BufferedReader response = new BufferedReader(
-                    new InputStreamReader(con.getInputStream()));
+            //Reading the response from the connection.
+            StringBuffer responseContent = GetResponse(con);
 
-            StringBuffer responseContent = new StringBuffer();
-            String line = response.readLine();
-            while (line != null) {
-                responseContent.append(line);
-                line = response.readLine();
-            }
-            response.close();
-            con.disconnect();
+            //Parsing the list of suggestions from the response.
             suggestionList = ParseJSON(responseContent);
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
         return suggestionList;
     }
 
-    private static List<String> ParseJSON(StringBuffer content) {
-        List<String> suggestionList = Arrays.asList();
-        JsonObject jsonContent = new JsonObject();
-        jsonContent.getAsJsonObject(content.toString());
 
-        JsonArray suggestions = jsonContent.getAsJsonArray("suggestions");
+    private static StringBuffer GetResponse(HttpURLConnection con) throws IOException{
+        BufferedReader response = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+
+        StringBuffer responseContent = new StringBuffer();
+        String line = response.readLine();
+        while (line != null) {
+            responseContent.append(line);
+            line = response.readLine();
+        }
+        response.close();
+        con.disconnect();
+
+        return responseContent;
+    }
+
+
+    /**
+     * Parse the list of suggestions from the JSON in the HTTP response.
+     * @param content represents the buffer holding the JSON response to parse.
+     */
+    private static ArrayList<String> ParseJSON(StringBuffer content) {
+        ArrayList<String> suggestionList = new ArrayList<>();
+        JsonParser parser = new JsonParser();
+        JsonObject suggestionObject = parser.parse(content.toString()).getAsJsonObject();
+
+        JsonArray suggestions = suggestionObject.getAsJsonArray("suggestions");
         for (JsonElement value : suggestions) {
-            suggestionList.add(value.toString());
+            suggestionList.add(value.toString().replace("\"", ""));
         }
         return suggestionList;
     }
--- src/test/java/odms/data/MedicationDataIOTest.java
+++ src/test/java/odms/data/MedicationDataIOTest.java
@@ -1,22 +1,49 @@
 package odms.data;
 
+import static odms.data.MedicationDataIO.GetSuggestionList;
 import static org.junit.Assert.*;
-
-import org.junit.After;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import org.junit.Before;
 import org.junit.Test;
 
 public class MedicationDataIOTest {
 
-    @Before
-    public void setUp() throws Exception {
-    }
+    private String substring1;
+    private String substring2;
+    private String substring3;
+    private Object[] expectedList1;
+    private Object[] expectedList2;
 
-    @After
-    public void tearDown() throws Exception {
+    @Before
+    public void setUp() {
+        //Test for null or empty substrings.
+        substring1 = "";
+        substring2 = null;
+        expectedList1 = Collections.emptyList().toArray();
+
+        //Test for substring with valid value.
+        substring3 = "res";
+        expectedList2 = new ArrayList<>(Arrays.asList("Reserpine", "Resectisol", "Resectisol in plastic container",
+                "Restoril", "Rescriptor", "Restasis", "Rescula", "Reserpine and hydrochlorothiazide",
+                "Reserpine, hydralazine hydrochloride and hydrochlorothiazide",
+                "Reserpine, hydrochlorothiazide, and hydralazine hydrochloride",
+                "Reserpine and hydrochlorothiazide-50", "Reserpine and hydroflumethiazide",
+                "Resporal")).toArray();
     }
 
     @Test
-    public void getSuggestionList() {
+    public void testEmptyOrNullStringGetSuggestionList() throws IOException {
+        //Test for null or empty substrings.
+        assertArrayEquals(expectedList1, GetSuggestionList(substring1).toArray());
+        assertArrayEquals(expectedList1, GetSuggestionList(substring2).toArray());
+    }
+
+    @Test
+    public void testValidStringGetSuggestionList() throws IOException {
+        //Test for substring with valid value.
+        assertArrayEquals(expectedList2, GetSuggestionList(substring3).toArray());
     }
 }
