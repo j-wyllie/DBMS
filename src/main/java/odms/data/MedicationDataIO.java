49eacf069c97f3ee0079bf99727f073e3037665c
mki58 <mki58@uclive.ac.nz>
2018-04-29 23:17:39 +1200

    Added functionality for drug interaction methods.
    Added method to parse through age data from the JSON response,
    getDrugInteractions now returns all the data that is needed. Test is
    written but failing due to no handling for response codes from the
    server. Will fix next commit and push test with that fix.
    
    #story[556]

--- src/main/java/odms/data/MedicationDataIO.java
+++ src/main/java/odms/data/MedicationDataIO.java
@@ -15,12 +15,23 @@ import java.util.Map;
 
 public class MedicationDataIO {
 
+    /**
+     * Get Map of interactions between two drugs. The values of each key is the time frame after taking both drugs in
+     * which the interaction may occur. Age and gender is used to get the most relevant interactions for a profile.
+     * @param drug1 Name of first drug.
+     * @param drug2 Name of second drug.
+     * @param gender gender of profile request is made for.
+     * @param age age of profile request is made for.
+     * @return Map<String, String> keys are valid interactions and values are duration of time after which an
+     * interaction may occur.
+     * @throws IOException
+     */
     public static Map<String, String> getDrugInteractions(String drug1, String drug2, String gender, int age) throws IOException {
         Map<String, String> interactions;
         interactions = new HashMap<>();
 
 
-        if (!(drug1 == null || drug1 == "" || drug2 == null || drug2 == "")) {
+        if (!(drug1 == null || drug1.equals("") || drug2 == null || drug2.equals(""))) {
             String urlString = String
                     .format("https://www.ehealthme.com/api/v1/drug-interaction/%s/%s/", drug1, drug2);
             URL url = new URL(urlString);
@@ -73,6 +84,7 @@ public class MedicationDataIO {
         JsonParser parser = new JsonParser();
         interactions = new HashMap<>();
         JsonObject results = parser.parse(content.toString()).getAsJsonObject();
+
         JsonObject genderInteractions = results.get("gender_interaction").getAsJsonObject();
         if (!(gender.equals("male") || gender.equals("female"))) {
             interactions.putAll(parseGenderInteractionsJSON(interactions, genderInteractions, "male"));
@@ -80,6 +92,10 @@ public class MedicationDataIO {
         } else {
             interactions.putAll(parseGenderInteractionsJSON(interactions, genderInteractions, gender));
         }
+
+        JsonObject interactionAge = results.get("age_interaction").getAsJsonObject();
+        interactions = parseInteractionAgeJSON(interactions, interactionAge, age);
+
         JsonObject interactionDuration = results.get("duration_interaction").getAsJsonObject();
         interactions = parseInteractionDurationJSON(interactions, interactionDuration);
         return interactions;
@@ -89,14 +105,14 @@ public class MedicationDataIO {
      * Parse JSON object that contains drug interactions based on male or female genders.
      * @param interactions Map of drug interactions, keys are valid interactions and values are duration of time after
      *                     which an interaction may occur.
-     * @param genderInteractions JSONObject that contains two JSONArrays, male or female. Arrays contain drug
+     * @param interactionGender JSONObject that contains two JSONArrays, male or female. Arrays contain drug
      *                           interactions
-     * @param gender gender of current profile logged in.
+     * @param gender gender of profile request is made for.
      * @return Map<String, String> keys are valid interactions and values are empty strings.
      */
     private static Map<String, String> parseGenderInteractionsJSON(Map<String, String> interactions,
-                                                                   JsonObject genderInteractions, String gender) {
-        JsonArray genderInteractionsArray = genderInteractions.get(gender).getAsJsonArray();
+                                                                   JsonObject interactionGender, String gender) {
+        JsonArray genderInteractionsArray = interactionGender.get(gender).getAsJsonArray();
         for (JsonElement value : genderInteractionsArray) {
             if (!interactions.containsKey(value.toString())) {
                 interactions.put(value.toString(), "");
@@ -106,25 +122,75 @@ public class MedicationDataIO {
     }
 
     /**
-     * Parse JSON object that contains drug interactions based on male or female genders.
+     *
      * @param interactions Map of drug interactions, keys are valid interactions and values are duration of time after
      *                     which an interaction may occur.
-     * @param interactionDuration
-     * @return
+     * @param interactionAge JSON object that contains arrays of interactions, each array represents the age group in
+     *                       which interactions may occur.
+     * @param age age of profile request is made for.
+     * @return Map<String, String> keys are valid interactions and values are empty strings.
      */
-    private static Map<String, String> parseInteractionDurationJSON(Map<String, String> interactions,
-                                                                    JsonObject interactionDuration) {
-        Map<String, String> result;
-        result = new HashMap<>();
+    private static Map<String, String> parseInteractionAgeJSON(Map<String, String> interactions,
+                                                               JsonObject interactionAge, int age) {
+        String ageGroup;
+        int lowerBound;
+        int upperBound;
+        String[] ageGroupArray;
+        JsonArray interactionArray;
 
-        for (Map.Entry<String, JsonElement> entry : interactionDuration.entrySet()) {
-            JsonArray interactionArray = entry.getValue().getAsJsonArray();
-            for (JsonElement value : interactionArray) {
-                if (interactions.containsKey(value.toString())) {
-                    result.put(value.toString(), entry.getKey());
+        for (Map.Entry<String, JsonElement> entry : interactionAge.entrySet()) {
+            ageGroup = entry.getKey();
+            if (ageGroup.equals("nan")) {
+                return interactions;
+            } else if (ageGroup.equals("60+") && age > 60) {
+                interactionArray = entry.getValue().getAsJsonArray();
+                for (JsonElement value : interactionArray) {
+                    interactions.put(value.toString(), "");
+                }
+                return interactions;
+            } else {
+                ageGroupArray = ageGroup.split("-");
+                lowerBound = Integer.parseInt(ageGroupArray[0]);
+                upperBound = Integer.parseInt(ageGroupArray[1]);
+                if (age >= lowerBound && age <= upperBound) {
+                    interactionArray = entry.getValue().getAsJsonArray();
+                    for (JsonElement value : interactionArray) {
+                        interactions.put(value.toString(), "");
+                    }
+                    return interactions;
                 }
             }
         }
-        return result;
+        return interactions;
+    }
+
+    /**
+     * Parse JSON object that contains drug interactions based on time of taking the drugs.
+     * @param interactions Map of drug interactions, keys are valid interactions and values are duration of time after
+     *                     which an interaction may occur.
+     * @param interactionDuration JSON object that contains arrays of interactions, each array represents the time frame
+     *                            in which the interaction may occur.
+     * @return Map<String, String> keys are valid interactions and values are duration of time after which an
+     * interaction may occur.
+     */
+    private static Map<String, String> parseInteractionDurationJSON(Map<String, String> interactions,
+                                                                    JsonObject interactionDuration) {
+        for (Map.Entry<String, JsonElement> entry : interactionDuration.entrySet()) {
+            JsonArray interactionArray = entry.getValue().getAsJsonArray();
+            for (JsonElement value : interactionArray) {
+                if (interactions.containsKey(value.toString()) && interactions.get(value.toString()).equals("")) {
+                    interactions.put(value.toString(), entry.getKey());
+                }
+            }
+        }
+
+        // If interaction did not have a duration and was not listed in 'not specified' JSON array, it is given the
+        // value 'not specified'
+        for (Map.Entry<String, String> entry : interactions.entrySet()) {
+            if (entry.getValue().equals("")) {
+                interactions.put(entry.getKey(), "not specified");
+            }
+        }
+        return interactions;
     }
 }
