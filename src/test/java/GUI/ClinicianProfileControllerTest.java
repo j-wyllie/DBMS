a271b75db346071444b1b6e91e44f17804da6a58
Lachlan Brewster <lbr63@uclive.ac.nz>
2018-04-30 14:01:21 +1200

    added tests for transplant table and refresh button, specifically for editing a profile through clicking on a transplant in the transplant table, making sure the correct profile is opened when clicking on the transplant table, and making sure the refresh button updates the search table, yet to add a test for the refresh button on the transplant table  #test #story[635]

--- src/test/java/GUI/ClinicianProfileControllerTest.java
+++ src/test/java/GUI/ClinicianProfileControllerTest.java
@@ -16,6 +16,7 @@ import javafx.stage.Window;
 import odms.controller.ClinicianProfileController;
 import odms.controller.GuiMain;
 import odms.controller.LoginController;
+import odms.profile.Organ;
 import odms.profile.Profile;
 import org.junit.*;
 import org.testfx.api.FxToolkit;
@@ -32,6 +33,7 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import static org.testfx.api.FxToolkit.registerPrimaryStage;
 import static org.testfx.api.FxToolkit.setupScene;
@@ -78,6 +80,74 @@ public class ClinicianProfileControllerTest extends ApplicationTest {
     private void logInClinician() {
         clickOn("#usernameField").write("0");
         clickOn("#loginButton");
+    }
+
+    /**
+     * Checks that the refresh button works correctly, ie updates the search table with up to date
+     * data when clicked
+     */
+    @Test
+    public void refreshButtonTest() {
+
+        //open up the first donor
+        clickOn("#searchTab");
+        Scene sceneSearchTab = getTopModalStage();
+        doubleClickOn(row("#searchTable", 0));
+        Scene sceneProfile = getTopModalStage();
+
+        Label userIdLabel = (Label) sceneProfile.lookup("#userIdLabel");
+        Integer userId = Integer.parseInt(userIdLabel.getText().substring(10)); //gets id of user being edited.
+
+        String originalGivenNames = ((Label) sceneProfile.lookup("#givenNamesLabel")).getText().substring(14);
+        String originalLastNames = ((Label) sceneProfile.lookup("#lastNamesLabel")).getText().substring(11);
+        //opening edit tab
+        clickOn((sceneProfile.lookup("#editButton")));
+
+        //editing donor
+        Scene sceneEdit = getTopModalStage();
+        TextField givenNames = (TextField) sceneEdit.lookup("#givenNamesField");
+        TextField lastNames = (TextField) sceneEdit.lookup("#lastNamesField");
+        clickOn(givenNames).eraseText(originalGivenNames.length()).write("John");
+        clickOn(lastNames).eraseText(originalLastNames.length()).write("Smith");
+
+        Button saveButton = (Button) sceneEdit.lookup("#saveButton");
+        clickOn(saveButton);
+
+        Stage stage = getAlertDialogue();
+        DialogPane dialogPane = (DialogPane) stage.getScene().getRoot();
+        Button yesButton = (Button) dialogPane.lookupButton(ButtonType.YES);
+        clickOn(yesButton);
+
+        clickOn(sceneSearchTab.lookup("#searchTab"));
+        Button refreshButton = (Button) sceneSearchTab.lookup("#refreshButton");
+        clickOn(refreshButton);
+
+        TableView searchTable = getTableView("#searchTable");
+        Profile profile = (Profile) searchTable.getItems().get(0);
+        String fullNameNew = profile.getFullName();
+
+        assertEquals("John Smith", fullNameNew);
+
+
+        //reset user through GUI
+        //open edit donor back up
+        sceneProfile = getTopModalStage();
+        clickOn(sceneProfile.lookup("#editButton"));
+
+        sceneEdit = getTopModalStage();
+        givenNames = (TextField) sceneEdit.lookup("#givenNamesField");
+        lastNames = (TextField) sceneEdit.lookup("#lastNamesField");
+        clickOn(givenNames).eraseText(4).write(originalGivenNames);
+        clickOn(lastNames).eraseText(5).write(originalLastNames);
+
+        Button saveButton2 = (Button) sceneEdit.lookup("#saveButton");
+        clickOn(saveButton2);
+
+        Stage stage2 = getAlertDialogue();
+        DialogPane dialogPane3 = (DialogPane) stage2.getScene().getRoot();
+        yesButton = (Button) dialogPane3.lookupButton(ButtonType.YES);
+        clickOn(yesButton);
+
     }
 
     /**
@@ -162,6 +232,96 @@ public class ClinicianProfileControllerTest extends ApplicationTest {
         yesButton = (Button) dialogPane3.lookupButton(ButtonType.YES);
         clickOn(yesButton);
     }
+
+
+
+    /**
+     * Checks that the correct profile is opened from the transplant table.
+     */
+    @Test
+    public void openTransplantProfileTest() {
+        clickOn("#transplantTab");
+        TableView transplantTable = getTableView("#transplantTable");
+        Profile firstDonor = ((Map.Entry<Profile, Organ>) transplantTable.getItems().get(0)).getKey();
+
+        doubleClickOn(row("#transplantTable", 0));
+        //opening the first donor
+        Scene scene = getTopModalStage();
+        Label profileName = (Label) scene.lookup("#donorFullNameLabel");
+        assertEquals(firstDonor.getFullName(), profileName.getText()); //checks name label is equal
+    }
+
+    /**
+     * Tests that a profile can be opened by a clinician via transplant table in transplant tab,
+     * and that the name can be successfully updated. The name is checked in the database and in the GUI to make sure it updates.
+     * Changes the donor back to the original.
+     */
+    @Test
+    public void editTransplantsProfileTest() {
+        //open up the first donor
+
+        clickOn("#transplantTab");
+        //TableView transplantTable = getTableView("#transplantTable");
+        //Profile firstDonor = ((Map.Entry<Profile, Organ>) transplantTable.getItems().get(0)).getKey();
+
+        doubleClickOn(row("#transplantTable", 0));
+        Scene scene = getTopModalStage();
+
+        Label userIdLabel = (Label) scene.lookup("#userIdLabel");
+        Integer userId = Integer.parseInt(userIdLabel.getText().substring(10)); //gets id of user being edited.
+
+        String originalGivenNames = ((Label) scene.lookup("#givenNamesLabel")).getText().substring(14);
+        String originalLastNames = ((Label) scene.lookup("#lastNamesLabel")).getText().substring(11);
+        //opening edit tab
+        clickOn((scene.lookup("#editButton")));
+
+        //editing donor
+        Scene scene2 = getTopModalStage();
+        TextField givenNames = (TextField) scene2.lookup("#givenNamesField");
+        TextField lastNames = (TextField) scene2.lookup("#lastNamesField");
+        clickOn(givenNames).eraseText(originalGivenNames.length()).write("Bob");
+        clickOn(lastNames).eraseText(originalLastNames.length()).write("Seger");
+
+        Button saveButton = (Button) scene2.lookup("#saveButton");
+        clickOn(saveButton);
+
+        Stage stage = getAlertDialogue();
+        DialogPane dialogPane = (DialogPane) stage.getScene().getRoot();
+        Button yesButton = (Button) dialogPane.lookupButton(ButtonType.YES);
+        clickOn(yesButton);
+
+        //checks database has been updated
+        assertEquals("Bob", GuiMain.getCurrentDatabase().getProfile(userId).getGivenNames());
+        assertEquals("Seger", GuiMain.getCurrentDatabase().getProfile(userId).getLastNames());
+
+        //checks GUI has been updated.
+        scene2 = getTopModalStage();
+        Label updatedGivenNames = (Label) scene2.lookup("#givenNamesLabel");
+        Label updatedLastNames = (Label) scene2.lookup("#lastNamesLabel");
+        assertEquals("Bob", updatedGivenNames.getText().substring(14));
+        assertEquals("Seger", updatedLastNames.getText().substring(11));
+
+        //reset user through GUI
+        //open edit donor back up
+        Scene donorScene = getTopModalStage();
+        //editDonorButton = (Button) searchedDonorScene.lookup("#editDonorButton2");
+        clickOn(donorScene.lookup("#editButton"));
+
+        scene2 = getTopModalStage();
+        givenNames = (TextField) scene2.lookup("#givenNamesField");
+        lastNames = (TextField) scene2.lookup("#lastNamesField");
+        clickOn(givenNames).eraseText(3).write(originalGivenNames);
+        clickOn(lastNames).eraseText(5).write(originalLastNames);
+
+        Button saveButton2 = (Button) scene2.lookup("#saveButton");
+        clickOn(saveButton2);
+
+        Stage stage2 = getAlertDialogue();
+        DialogPane dialogPane3 = (DialogPane) stage2.getScene().getRoot();
+        yesButton = (Button) dialogPane3.lookupButton(ButtonType.YES);
+        clickOn(yesButton);
+    }
+
 
     /**
      * @param tableSelector The id of the table to be used
