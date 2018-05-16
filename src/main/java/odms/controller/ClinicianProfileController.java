eefc2a8bbd836a138e523e6f27570a555abfebe4
mki58 <mki58@uclive.ac.nz>
2018-05-16 15:49:27 +1200

    Changed behaviour of search table. It now starts blank, displaying the number of profiles.
    Changed behaviour of search algorithm. It now searches from start of
    first name, and only searches first name. This is a work in progress.
    
    #story[736]

--- src/main/java/odms/controller/ClinicianProfileController.java
+++ src/main/java/odms/controller/ClinicianProfileController.java
@@ -6,7 +6,6 @@ import static odms.controller.UndoRedoController.undo;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
