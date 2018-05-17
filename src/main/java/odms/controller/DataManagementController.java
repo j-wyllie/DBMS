2e7b03ed68a7ac64f35725bdf48a8ff7b2000558
Jack <jha233@uclive.ac.nz>
2018-05-17 13:45:24 +1200

    Added filechooser and an extension filter. Found that passing the filepath to the dataloader does not work - FileNotFound
     #story[741]

--- src/main/java/odms/controller/DataManagementController.java
+++ src/main/java/odms/controller/DataManagementController.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import odms.data.ProfileDataIO;
+
+import java.io.File;
 
 public class DataManagementController {
 
@@ -14,9 +17,11 @@ public class DataManagementController {
     public void handleImportSavedDataClicked(ActionEvent actionEvent) {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open Resource File");
-
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("JSON file(*.json)",
+                "*.json");
+        fileChooser.getExtensionFilters().add(extFilter);
         Stage stage = (Stage) dataManagementAp.getScene().getWindow();
-        fileChooser.showOpenDialog(stage);
-
+        File file = fileChooser.showOpenDialog(stage);
+        GuiMain.setCurrentDatabase(ProfileDataIO.loadData(file.getPath()));
     }
 }
