d205c23b620bab735cf8502ea8781842e3029c47
Alex <ami80@uclive.ac.nz>
2018-05-21 13:25:10 +1200

    Improve InvalidEntry alert to display meaningful error to the user. - AlertController: Overloaded function added to allow custom message (Typical usage is to get the message from the exception.) - ProfileCreateController: Improved checking and errors presented to user. #story[746] #fix

--- src/main/java/odms/controller/ProfileCreateController.java
+++ src/main/java/odms/controller/ProfileCreateController.java
@@ -34,6 +34,33 @@ public class ProfileCreateController extends CommonController {
     @FXML
     private TextField irdField;
 
+    private String checkDetailsEntered() {
+        if (givenNamesField.getText().trim().equals("")) {
+            return "Please enter Given Name(s)";
+        }
+        if (surnamesField.getText().trim().equals("")) {
+            return "Please enter Surname(s)";
+
+        }
+        if (dobField.getText().trim().equals("")) {
+            return "Please enter a Date of Birth";
+
+        }
+        if (irdField.getText().trim().equals("")) {
+            return "Please an IRD number";
+        } else {
+            return "";
+        }
+    }
+
+    public void initialize() {
+        irdField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                irdField.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
+    }
+
     /**
      * Scene change to profile profile view if all required fields are filled in.
      * @param event clicking on the create new account button.
@@ -41,22 +68,25 @@ public class ProfileCreateController extends CommonController {
      */
     @FXML
     private void handleCreateAccountButtonClicked(ActionEvent event) throws IOException {
-        if(givenNamesField.getText().trim().equals("") || surnamesField.getText().trim().equals("") ||
-                dobField.getText().trim().equals("") || irdField.getText().trim().equals("")) {
-            invalidEntry();
+        String detailsCheckString = checkDetailsEntered();
+
+        if (!detailsCheckString.equals("")) {
+            invalidEntry(detailsCheckString);
         } else {
             try {
                 String givenNames = givenNamesField.getText();
                 String surnames = surnamesField.getText();
                 String dob = dobField.getText();
-                String ird = irdField.getText();
+                Integer ird = Integer.valueOf(irdField.getText());
 
-                Profile newProfile = new Profile(givenNames, surnames, dob, Integer.valueOf(ird));
+                Profile newProfile = new Profile(givenNames, surnames, dob, ird);
 
                 currentDatabase.addProfile(newProfile);
                 ProfileDataIO.saveData(currentDatabase);
 
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/ProfileDisplay.fxml"));
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
+                        "/view/ProfileDisplay.fxml")
+                );
 
                 Scene scene = new Scene(fxmlLoader.load());
                 ProfileDisplayController controller = fxmlLoader.getController();
@@ -67,9 +97,18 @@ public class ProfileCreateController extends CommonController {
 
                 appStage.setScene(scene);
                 appStage.show();
+            } catch (NumberFormatException e) {
+                if (irdField.getText().length() > 9) {
+                    invalidEntry("Entered IRD number is too long.\nPlease enter up to 9 digits");
+                } else {
+                    invalidEntry("Invalid IRD number entered");
+                }
             } catch (IllegalArgumentException e) {
-                //show error window.
-                invalidEntry();
+                if (!e.getMessage().equals("")) {
+                    invalidEntry(e.getMessage());
+                } else {
+                    invalidEntry();
+                }
             } catch (IrdNumberConflictException e) {
                 invalidIrd();
             } catch (ArrayIndexOutOfBoundsException e) {
