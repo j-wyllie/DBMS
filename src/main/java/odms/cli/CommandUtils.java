7e6fa1d511e68226aaaa210d98d530e0afc2f496
Lewis <lwh34@uclive.ac.nz>
2018-05-21 20:01:22 +1200

    Same bug but for receivers

--- src/main/java/odms/cli/CommandUtils.java
+++ src/main/java/odms/cli/CommandUtils.java
@@ -164,7 +164,7 @@ public class CommandUtils {
      */
     static void addReceiverOrgansBySearch(ProfileDatabase currentDatabase, String expression) {
         String[] organList = expression.substring(
-                expression.lastIndexOf("=") + 1).replace("\"", "").split(",");
+                expression.lastIndexOf("=") + 1).replace("\"", "").replace(" ", "").split(",");
 
         if (expression.substring(0, expression.lastIndexOf('>')).lastIndexOf("=") ==
                 expression.substring(0, expression.lastIndexOf('>')).indexOf("=")) {
