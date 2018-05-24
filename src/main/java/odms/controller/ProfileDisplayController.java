469d7a6d0818230485cfe97c08ced7b09a613163
Tim Hamblin <tha110@uclive.ac.nz>
2018-05-24 16:09:24 +1200

    Fixed the login button being visible when it shouldn't be (my bad) #story[746]

--- src/main/java/odms/controller/ProfileDisplayController.java
+++ src/main/java/odms/controller/ProfileDisplayController.java
@@ -1169,6 +1169,7 @@ public class ProfileDisplayController extends CommonController {
         allConditions.addAll(convertConditionObservableToArray(
                 pastConditionsTable.getSelectionModel().getSelectedItems()));
 
+        hideItems();
         disableButtonsIfNoItems(allConditions);
 
         ArrayList<Drug> drugs = convertObservableToArray(
