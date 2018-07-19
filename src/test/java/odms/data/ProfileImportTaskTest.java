98edf816017a8d4226c1a6c38d6a7581374571f5
Jack Hay <jha233@uclive.ac.nz>
2018-07-19 18:25:09 +1200

    Deleted tests that were commented out for the code review #story[826]

--- src/test/java/odms/data/ProfileImportTaskTest.java
+++ src/test/java/odms/data/ProfileImportTaskTest.java
@@ -9,63 +9,33 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ProfileImportTaskTest {
-    private ProfileImportTask pit;
-
+    private ProfileImportTask profileImportTask;
 
     @Before
     public void setup() {
         File file = new File("./src/test/java/odms/data/profileTestData/CSVProfiles1.csv");
-        pit = new ProfileImportTask(file);
+        profileImportTask = new ProfileImportTask(file);
     }
 
-//    @Test
-//    public void testLoad1ProfileFromCSV() throws InvalidFileException {
-//        ProfileDatabase loadedDb;
-//        File file = new File("./src/test/java/odms/data/profileTestData/CSVProfiles1.csv");
-//        ProfileImportTask pit = new ProfileImportTask(file);
-//        loadedDb = pit.loadDataFromCSV(file);
-//        assertEquals(1, loadedDb.getProfiles(false).size());
-//    }
-
-//    @Test
-//    public void testLoad100ProfilesFromCSV() throws InvalidFileException {
-//        ProfileDatabase loadedDb;
-//        File file = new File("./src/test/java/odms/data/profileTestData/CSVProfiles100.csv");
-//        loadedDb = ProfileDataIO.loadDataFromCSV(file);
-//        assertEquals(100, loadedDb.getProfiles(false).size());
-//    }
-//
-//    @Test
-//    public void testCSVToProfileConverterInvalid() {
-//        ProfileDatabase loadedDb;
-//
-//        File file = new File("./src/test/java/odms/data/profileTestData/CSVProfilesInvalid.csv");
-//        try {
-//            loadedDb = ProfileDataIO.loadDataFromCSV(file);
-//        } catch (InvalidFileException e) {
-//            assert(true);
-//        }
-//    }
-//
     @Test
     public void testInvalidNHI() {
         // Only one assert going on here, nothing to see
         String invalidNHI = "123TREE";
-        assertFalse(pit.isValidNHI(invalidNHI));
+        assertFalse(profileImportTask.isValidNHI(invalidNHI));
 
         invalidNHI = "ABO1234";
-        assertFalse(pit.isValidNHI(invalidNHI));
+        assertFalse(profileImportTask.isValidNHI(invalidNHI));
 
         invalidNHI = "ABI1234";
-        assertFalse(pit.isValidNHI(invalidNHI));
+        assertFalse(profileImportTask.isValidNHI(invalidNHI));
 
         invalidNHI = "AB1234";
-        assertFalse(pit.isValidNHI(invalidNHI));
+        assertFalse(profileImportTask.isValidNHI(invalidNHI));
     }
 
     @Test
     public void testValidNHI() {
         String validNHI = "ABC1234";
-        assertTrue(pit.isValidNHI(validNHI));
+        assertTrue(profileImportTask.isValidNHI(validNHI));
     }
 }
