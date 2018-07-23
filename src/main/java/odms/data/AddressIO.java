6b4c5be63f8847bb49cba8ec6897877f31ce92bb
Alex <ami80@uclive.ac.nz>
2018-07-24 10:24:37 +1200

    Checkstyle fixes. #story[861] #implement #fix

--- src/main/java/odms/data/AddressIO.java
+++ src/main/java/odms/data/AddressIO.java
@@ -1,27 +1,30 @@
 package odms.data;
 
-
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import odms.enums.CountriesEnum;
-
-import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
+import java.util.List;
+import odms.enums.CountriesEnum;
 
 public class AddressIO {
     private static final  String API_URL = "https://maps.googleapis.com/maps/api/";
     private static String key;
 
-    public static boolean checkValidCountry(String address, ArrayList<CountriesEnum> validCountries) {
-        //todo either use one key or come up with a way to use a few
+    private AddressIO() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean checkValidCountry(String address, List<CountriesEnum> validCountries) {
+        // TODO either use one key or come up with a way to use a few
         key = "AIzaSyCfq6coJWIFGQusltLJCA8tZMt9cjouzLw";
-        String query = API_URL+"geocode/json?address="+address.replace(" ","+")+"&key="+key;
+        String query = API_URL +
+                "geocode/json?address=" +
+                address.replace(" ","+") +
+                "&key=" + key;
         System.out.println(query);
         try {
             URL url = new URL(query);
