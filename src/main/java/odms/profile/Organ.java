b7c947fb4f1e628b5ac83a615f2686a7b620bcad
Jack <jha233@uclive.ac.nz>
2018-07-23 13:25:59 +1200

    Created Organ class and wrote tests to check that the correct date was set on instantiation.
    #story[869]
    #pair[jha233,jwy31]

---
+++ src/main/java/odms/profile/Organ.java
@@ -0,0 +1,26 @@
+package odms.profile;
+
+import java.time.LocalDate;
+import odms.enums.OrganEnum;
+
+/**
+ * Organ class that contains the organ type and stores the current date when created.
+ */
+public class Organ {
+
+    OrganEnum organType;
+    LocalDate dateOfRegistration;
+
+    public Organ(OrganEnum organType) {
+        this.organType = organType;
+        this.dateOfRegistration = LocalDate.now();
+    }
+
+    public LocalDate getDateOfRegistration() {
+        return dateOfRegistration;
+    }
+
+    public OrganEnum getOrganType() {
+        return organType;
+    }
+}
