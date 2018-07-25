b96080d7ede3e78155c8a1e093995a037696d9bc
Joshua <jwy31@uclive.ac.nz>
2018-07-26 00:40:19 +1200

    -Implemented tests for MySQLUserDAO
    #story[896]

--- src/test/java/odms/dao/MySqlProfileDAOTest.java
+++ src/test/java/odms/dao/MySqlProfileDAOTest.java
@@ -16,12 +16,18 @@ public class MySqlProfileDAOTest {
     private Profile testProfile0 = new Profile("Joshua", "Wyllie", LocalDate.of(1997, 7, 18), "ABC1234");
     private Profile testProfile1 = new Profile("Jack", "Hay", LocalDate.of(1998, 2, 27), "CBA43211");
 
+    /**
+     * Sets the Database to the test database and Initialises the DBO
+     */
     @Before
     public void setUp() {
         DatabaseConnection.setConfig("/config/db_test.config");
         mySqlProfileDAO = new MySqlProfileDAO();
     }
 
+    /**
+     * Tests adding and getting a profile by id
+     */
     @Test
     public void testAddGet() {
         testProfile0.setId(42);
@@ -31,6 +37,9 @@ public class MySqlProfileDAOTest {
         Assert.assertEquals(testProfile0, outProfile);
     }
 
+    /**
+     * Tests adding multiple profiles and the getAll function
+     */
     @Test
     public void testGetAll() {
         mySqlProfileDAO.add(testProfile0);
@@ -42,6 +51,9 @@ public class MySqlProfileDAOTest {
         Assert.assertEquals(allOutProfiles, allInProfiles);
     }
 
+    /**
+     * Tests removing a profile
+     */
     @Test
     public void testRemove() {
         mySqlProfileDAO.remove(testProfile0);
@@ -49,6 +61,9 @@ public class MySqlProfileDAOTest {
         Assert.assertEquals(allProfiles.get(0), testProfile1);
     }
 
+    /**
+     * Sets the database back to the production database
+     */
     @After
     public void cleanUp() {
         DatabaseConnection.setConfig("/config/db.config");
