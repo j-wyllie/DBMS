36609fe61e6e4360125bf51d485e5408172a403c
Tim Hamblin <tha110@uclive.ac.nz>
2018-07-26 12:51:13 +1200

    Merged in dev. Resolved merge conflicts & errors. #story[896] #pair[ami80,tha110] #fix

--- src/main/java/odms/dao/MedicationInteractionsDAO.java
+++ src/main/java/odms/dao/MedicationInteractionsDAO.java
@@ -1,6 +1,5 @@
 package odms.dao;
 
-import odms.medications.Drug;
 import java.io.IOException;
 import java.util.Map;
 import odms.medications.Interaction;
@@ -8,52 +7,10 @@ import odms.medications.Interaction;
 public interface MedicationInteractionsDAO {
 
     /**
-     * Get all interactions between two drugs stored in the database.
-     * @param drugA of the interactions.
-     * @param drugB of the interactions
-     */
-    void get(Drug drugA, Drug drugB);
-
-    /**
-     * Add a new interaction between two drugs in the database.
-     * @param drugA to interact.
-     * @param drugB to interact.
-     * @param symptom of the drug interactions.
-     * @param duration of the drug interactions.
-     */
-    void add(Drug drugA, Drug drugB, String symptom, String duration);
-
-    /**
-     * Remove all interactions between two drugs.
-     * @param drugA of the interactions to remove.
-     * @param drugB of the interactions to remove.
-     */
-    void removeAll(Drug drugA, Drug drugB);
-
-    /**
-     * Remove a particular interaction between two drugs.
-     * @param drugA of the interaction to remove.
-     * @param drugB of the interaction to remove.
-     * @param symptom of the interaction to remove.
-     * @param duration of the interaction to remove.
-     */
-    void remove(Drug drugA, Drug drugB, String symptom, String duration);
-
-    /**
-     * Update a particular interaction between two drugs.
-     * @param drugA of the interaction to update.
-     * @param drugB of the interaction to update.
-     * @param symptom of the interaction to update.
-     * @param duration of the interaction to update.
-     */
-    void update(Drug drugA, Drug drugB, String symptom, String duration);
-
-    /**
      * Get all interaction data stored in the cache.
      * @return all interactions data.
      */
     Map<Integer, Interaction> getAll();
-
     /**
      * Get the interactions between two medications.
      * @param drugA is a interacting medication.
