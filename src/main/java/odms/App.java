42a132253e6cb76843dabe90891d0ce6ded242ce
bra65 <bra65@uclive.ac.nz>
2018-07-26 13:36:30 +1200

    Created query to add all the country enums to the database. #story[896] #pair[bra65,jha233]

--- src/main/java/odms/App.java
+++ src/main/java/odms/App.java
@@ -1,15 +1,23 @@
 package odms;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
 import javafx.application.Application;
 import odms.cli.CommandLine;
 import odms.controller.GuiMain;
 import odms.controller.HistoryController;
+import odms.dao.DatabaseConnection;
 import odms.data.ProfileDataIO;
 import odms.data.ProfileDatabase;
 import odms.data.UserDataIO;
 import odms.data.UserDatabase;
 
 import java.io.File;
+import odms.enums.CountriesEnum;
 
 public class App {
     private static final String DONOR_DATABASE = "example/example.json";
---
+++ src/main/java/odms/dao/CountryDAO.java
@@ -0,0 +1,9 @@
+package odms.dao;
+
+import java.util.List;
+
+public interface CountryDAO {
+
+    List<String> getAll(boolean allowed);
+
+}
