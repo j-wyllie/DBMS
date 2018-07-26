33900a87826b75bab828542543ccba100982922d
Jack <jha233@uclive.ac.nz>
2018-07-26 13:39:30 +1200

    Created dao functionality to store valid countries. #story[861] #pair[jha233,bra65]

--- src/main/java/odms/dao/CountryDAO.java
+++ src/main/java/odms/dao/CountryDAO.java
@@ -1,9 +1,20 @@
 package odms.dao;
 
 import java.util.List;
+import odms.enums.CountriesEnum;
 
 public interface CountryDAO {
 
-    List<String> getAll(boolean allowed);
+    /**
+     * Gets all countries from the database.
+     * @return a list of countries.
+     */
+    List<CountriesEnum> getAll();
 
+    /**
+     * Gets all valid or invalid countries from the database.
+     * @param valid true if valid countries are required.
+     * @return a list of countries.
+     */
+    List<CountriesEnum> getAll(boolean valid);
 }
--- src/main/java/odms/dao/MySqlCountryDAO.java
+++ src/main/java/odms/dao/MySqlCountryDAO.java
@@ -1,5 +1,63 @@
 package odms.dao;
 
-public class MySqlCountryDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import odms.enums.CountriesEnum;
 
+public class MySqlCountryDAO implements CountryDAO {
+
+    @Override
+    public List<CountriesEnum> getAll() {
+        List<CountriesEnum> countries = new ArrayList<>();
+        DatabaseConnection connectionInstance = DatabaseConnection.getInstance();
+        String query = "select * from countries;";
+        try {
+            Connection connection = connectionInstance.getConnection();
+
+            Statement stmt = connection.createStatement();
+            ResultSet result = stmt.executeQuery(query);
+
+            while (result.next()) {
+                countries.add(CountriesEnum.valueOf(result.getString("Name")));
+            }
+
+            stmt.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            System.out.println("Please enter a valid read-only query.");
+        }
+        return countries;
+    }
+
+    @Override
+    public List<CountriesEnum> getAll(boolean valid) {
+        List<CountriesEnum> countries = new ArrayList<>();
+        DatabaseConnection connectionInstance = DatabaseConnection.getInstance();
+        String query = "select * from countries where Valid = ?;";
+        try {
+            Connection connection = connectionInstance.getConnection();
+
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setBoolean(1, valid);
+
+            ResultSet result = stmt.executeQuery();
+
+            while (result.next()) {
+                countries.add(CountriesEnum.valueOf(result.getString("Name")));
+            }
+
+            stmt.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            System.out.println("Please enter a valid read-only query.");
+        }
+        return countries;
+    }
 }
