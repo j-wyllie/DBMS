224c77a0c197641353d3247a76b43de6227c6988
bra65 <bra65@uclive.ac.nz>
2018-08-11 11:41:19 +1200

    Implemented functions at endpoints to get and create a single profile. #implement #story[981]

--- server/src/main/java/server/controller/ProfileController.java
+++ server/src/main/java/server/controller/ProfileController.java
@@ -1,6 +1,7 @@
 package server.controller;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import org.sonar.api.internal.google.gson.Gson;
 import server.model.database.DAOFactory;
 import server.model.database.profile.ProfileDAO;
@@ -11,8 +12,10 @@ public class ProfileController {
 
     public static String getAll(Request req, Response res) {
         ProfileDAO database = DAOFactory.getProfileDao();
+        List<Profile> profiles = new ArrayList<>();
+
         try {
-            List<Profile> profiles = database.getAll();
+            profiles = database.getAll();
         } catch (SQLException e) {
             res.status(500);
             return e.getMessage();
@@ -28,11 +31,48 @@ public class ProfileController {
     }
 
     public static String get(Request req, Response res) {
-        return "Unimplemented";
+        ProfileDAO database = DAOFactory.getProfileDao();
+        Profile profile = null;
+
+        try {
+            profile = database.get(Integer.valueOf(req.params("id")));
+        } catch (SQLException e) {
+            res.status(500);
+            return e.getMessage();
+        }
+
+        Gson gson = new Gson();
+        String responseBody = gson.toJson(profile);
+
+        res.type("application/json");
+        res.status(200);
+
+        return responseBody;
     }
 
     public static String create(Request req, Response res) {
-        return "Unimplemented";
+        Gson gson = new Gson();
+        ProfileDAO database = DAOFactory.getProfileDao();
+        Profile newProfile = null;
+
+        try {
+            newProfile = gson.fromJson(req.body(), Profile.class);
+        } catch (Exception e) {
+            res.status(400);
+            return "Bad Request";
+        }
+
+        if (!(newProfile == null)) {
+            try {
+                database.add(newProfile);
+            } catch (SQLException e) {
+                res.status(500);
+                return "Internal Server Error";
+            }
+        }
+
+        res.status(201);
+        return "Profile Created";
     }
 
     public static String edit(Request req, Response res) {
