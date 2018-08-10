42d6df8cad10d8be274a454d681ccbea1e21de9e
bra65 <bra65@uclive.ac.nz>
2018-08-11 11:17:57 +1200

    Wrote controller function to return a list of all profiles in response body. #implement #story[981]

--- server/src/main/java/server/controller/ProfileController.java
+++ server/src/main/java/server/controller/ProfileController.java
@@ -1,12 +1,30 @@
 package server.controller;
 
+import java.sql.SQLException;
+import org.sonar.api.internal.google.gson.Gson;
+import server.model.database.DAOFactory;
+import server.model.database.profile.ProfileDAO;
 import spark.Request;
 import spark.Response;
 
 public class ProfileController {
 
     public static String getAll(Request req, Response res) {
-        return "Unimplemented";
+        ProfileDAO database = DAOFactory.getProfileDao();
+        try {
+            List<Profile> profiles = database.getAll();
+        } catch (SQLException e) {
+            res.status(500);
+            return e.getMessage();
+        }
+
+        Gson gson = new Gson();
+        String responseBody = gson.toJson(profiles);
+
+        res.type("application/json");
+        res.status(200);
+
+        return responseBody;
     }
 
     public static String get(Request req, Response res) {
