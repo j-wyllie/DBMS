931a2ba5546d27d41797fb199d3066b9a3c4685f
Tim Hamblin <tha110@uclive.ac.nz>
2018-08-13 00:16:46 +1200

    Implemented the countries GET endpoint. Not sure if this branch is running yet but I based my code on what I could see had been done for some of the other endpoints #story[945]

--- server/src/main/java/server/controller/CountriesController.java
+++ server/src/main/java/server/controller/CountriesController.java
@@ -1,15 +1,68 @@
 package server.controller;
 
+import java.util.List;
+import org.sonar.api.internal.google.gson.Gson;
+import server.model.database.DAOFactory;
+import server.model.database.country.CountryDAO;
 import spark.Request;
 import spark.Response;
 
 public class CountriesController {
 
+    /**
+     * Gets a list of all the countries in the database
+     * Takes an optional query parameter to define the validity of the country
+     * @param req The request sent to the endpoint
+     * @param res The response sent back
+     * @return The response body as a list of Json object of countries
+     */
     public static String getAll(Request req, Response res) {
+        Boolean valid = null;
+        List<String> countries;
+
+        // This query is optional so only assign it if it exists
+        if (req.queryMap().hasKey("valid")) {
+            valid = Boolean.valueOf(req.queryParams("valid"));
+        }
+
+        try {
+            countries = getCountries(valid);
+        } catch (Exception e) {
+            res.status(500);
+            return e.getMessage();
+        }
+
+        Gson gson = new Gson();
+        String responseBody = gson.toJson(countries);
+
+        res.type("application/json");
+        res.status(200);
+
+        return responseBody;
+    }
+
+    /**
+     * Updates a countries valid field to true or false
+     * @param req The request sent to the endpoint
+     * @param res The response sent back
+     * @return The response body
+     */
+    public static String edit(Request req, Response res) {
         return "Unimplemented";
     }
 
-    public static String edit(Request req, Response res) {
-        return "Unimplemented";
+    /**
+     * Gets the countries from the database
+     * @param valid optional query to define the validity of the countries
+     * @return A set of country names
+     */
+    private static List<String> getCountries(Boolean valid) {
+        CountryDAO countryDAO = DAOFactory.getCountryDAO();
+
+        if (valid == null) {
+            return countryDAO.getAll();
+        } else {
+            return countryDAO.getAll(valid);
+        }
     }
 }
