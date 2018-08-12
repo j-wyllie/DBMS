45f7d87186e7e2b64c33a5fe06d4da87c1c1b40d
Alex Miller <ami80@uclive.ac.nz>
2018-08-13 03:41:13 +1200

    Skeleton basic tests for ImageDataIO #story[981] #test

---
+++ src/test/java/odms/controller/data/ImageDataIOTest.java
@@ -0,0 +1,23 @@
+package odms.controller.data;
+
+import org.junit.Test;
+
+public class ImageDataIOTest {
+
+    @Test
+    public void testDeleteAndSaveImage() {
+
+    }
+
+    @Test
+    public void testDeleteImage() {
+
+    }
+
+    @Test
+    public void testGetImagePath() {
+        String expectedBasePath;
+
+    }
+
+}
