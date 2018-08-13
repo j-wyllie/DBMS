fb0f68dd8426fe7ae266ba5725b0579d6f5ea9fd
Matt <mki58@uclive.ac.nz>
2018-08-13 19:15:08 +1200

    Sorting out imported packages that are broken due to package spit.
    #story[981]

---
+++ client/src/main/java/odms/controller/profile/ProfileHistory.java
@@ -0,0 +1,31 @@
+package odms.controller.profile;
+
+import odms.controller.data.ProfileDataIO;
+
+public class ProfileHistory {
+
+    private odms.view.profile.ProfileHistory view;
+
+    public ProfileHistory(odms.view.profile.ProfileHistory v) {
+        view = v;
+    }
+
+    public String getHistory() {
+        String history = ProfileDataIO.getHistory();
+        history = history.replace(",", " ").replace("]", "")
+                .replace("[", "").replace("\\u003d", "=");
+        String[] histories = history.split("\"");
+
+        String historyDisplay = "";
+
+        for (String h : histories) {
+            if (!h.equals("") && h.contains("profile " + view.getProfile().getId() + " ")) {
+                historyDisplay += h + "\n";
+            }
+        }
+
+        return historyDisplay;
+    }
+}
+
+
