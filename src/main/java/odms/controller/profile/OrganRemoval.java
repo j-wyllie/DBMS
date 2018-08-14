5956f236eb109aaedba9f8e4b07904dd66e805a9
Matt <mki58@uclive.ac.nz>
2018-08-15 02:37:08 +1200

    Fixed bug that caused required organs to not be removed from a profile if they are marked as dead.
    #story[996]

--- src/main/java/odms/controller/profile/OrganRemoval.java
+++ src/main/java/odms/controller/profile/OrganRemoval.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 import odms.controller.CommonController;
+import odms.controller.database.DAOFactory;
 import odms.controller.history.CurrentHistory;
 import odms.model.enums.OrganEnum;
 import odms.model.profile.Profile;
@@ -34,7 +35,6 @@ public class OrganRemoval extends CommonController {
 
             case "Patient deceased":
                 view.removeAllOrgans();
-                //todo way to set time of death
                 view.getCurrentProfile().setDateOfDeath(LocalDateTime.from(view.getDOD()));
                 Set<OrganEnum> organsRequired = new HashSet<>(
                         view.getCurrentProfile().getOrgansRequired()
@@ -55,6 +55,7 @@ public class OrganRemoval extends CommonController {
         //todo fix generate update info into simpler solution
         //generateUpdateInfo("organsReceiving");
         for (OrganEnum organ : organs) {
+            DAOFactory.getOrganDao().removeRequired(p, organ);
             p.getOrgansRequired().remove(organ);
             odms.model.history.History action = new odms.model.history.History(
                     "profile ",
