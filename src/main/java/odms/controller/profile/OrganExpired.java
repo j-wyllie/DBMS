415ee702c1eae34c0e01d81e7ca0e68bebb37e11
Lewis W <lwh34@uclive.ac.nz>
2018-08-14 20:31:38 +1200

    Added the new files for story 48 #story[989]

---
+++ src/main/java/odms/controller/profile/OrganExpired.java
@@ -0,0 +1,33 @@
+package odms.controller.profile;
+
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import odms.controller.AlertController;
+import odms.controller.CommonController;
+import odms.controller.database.DAOFactory;
+import odms.controller.database.ProfileDAO;
+import odms.controller.history.CurrentHistory;
+import odms.model.enums.OrganEnum;
+import odms.model.profile.OrganConflictException;
+import odms.model.profile.Profile;
+
+/**
+ * Control access for items around the organ lists.
+ */
+public class OrganExpired extends CommonController {
+    private odms.view.profile.OrganExpired view;
+
+    public OrganExpired(odms.view.profile.OrganExpired view) {
+        this.view = view;
+    }
+
+
+
+
+
+}
