87f72cc3c6aa3d93bf4e8fc629b87e698add88ba
Matt King <mki58@uclive.ac.nz>
2018-08-28 13:25:14 +1200

    Added missing javaDoc
    #story[1117]

--- client/src/main/java/odms/controller/profile/OrganOverride.java
+++ client/src/main/java/odms/controller/profile/OrganOverride.java
@@ -5,13 +5,24 @@ import odms.controller.database.DAOFactory;
 
 import java.sql.SQLException;
 
+/**
+ * Controller class for the OrganOverride view class.
+ */
 public class OrganOverride extends CommonController {
-    odms.view.profile.OrganOverride view;
+    private odms.view.profile.OrganOverride view;
 
+    /**
+     * Sets the view variable.
+     * @param view OrganOverride view instance
+     */
     public OrganOverride(odms.view.profile.OrganOverride view) {
         this.view = view;
     }
 
+    /**
+     * Takes details from view fields and sends a request to the DAO to override an organ.
+     * @throws SQLException thrown if there are malformed details
+     */
     public void confirm() throws SQLException {
         Integer isExpired = 1;
         DAOFactory.getOrganDao().setExpired(view.getCurrentProfile(), view.getCurrentOrgan(), isExpired,
