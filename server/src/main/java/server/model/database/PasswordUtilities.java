fc7326c1fd9228da7373e464dbee5ce933ab56a0
Jack <jha233@uclive.ac.nz>
2018-09-06 10:44:18 +1200

    Trying to speed up tests/
    #story[1118]

--- server/src/main/java/server/model/database/PasswordUtilities.java
+++ server/src/main/java/server/model/database/PasswordUtilities.java
@@ -16,8 +16,8 @@ public final class PasswordUtilities {
     // The higher the number of ITERATIONS the more
     // expensive computing the hash is for us and
     // also for an attacker.
-    private static final int ITERATIONS = 20 * 100;
-    private static final int SALT_LEN = 32;
+    private static final int ITERATIONS = 20;
+    private static final int SALT_LEN = 16;
     private static final int DESIRED_KEY_LEN = 128;
     private static final String ALGORITHM = "SHA1PRNG";
 
