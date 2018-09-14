78b77c0f66ebec89a100b7894944edd8e9e62741
Tim Hamblin <tha110@uclive.ac.nz>
2018-09-15 11:19:26 +1200

    Merge branch 'C1_Donor_and_Receiver_Maps' of /home/cosc/student/tha110/IdeaProjects/team-200 with conflicts.

--- client/src/main/java/odms/view/user/OrganMap.java
+++ client/src/main/java/odms/view/user/OrganMap.java
@@ -3,100 +3,73 @@ package odms.view.user;
 import com.lynden.gmapsfx.GoogleMapView;
 import com.lynden.gmapsfx.MapComponentInitializedListener;
 import com.lynden.gmapsfx.javascript.event.UIEventType;
-import com.lynden.gmapsfx.javascript.object.GoogleMap;
-import com.lynden.gmapsfx.javascript.object.LatLong;
-import com.lynden.gmapsfx.javascript.object.MapOptions;
-import com.lynden.gmapsfx.javascript.object.MapTypeIdEnum;
-import com.lynden.gmapsfx.javascript.object.Marker;
-import com.lynden.gmapsfx.javascript.object.MarkerOptions;
+import com.lynden.gmapsfx.javascript.object.*;
 import com.lynden.gmapsfx.util.MarkerImageFactory;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.ResourceBundle;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import lombok.extern.slf4j.Slf4j;
 import odms.commons.model.profile.Profile;
 import odms.commons.model.user.User;
+import odms.controller.database.DAOFactory;
+import odms.controller.database.profile.ProfileDAO;
 import odms.view.CommonView;
+import lombok.extern.slf4j.Slf4j;
 
-/**
- * Tab containing the organ map.
- */
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.ResourceBundle;
+
 @Slf4j
 public class OrganMap extends CommonView implements Initializable, MapComponentInitializedListener {
 
-    private static final String RECEIVER_MARKER = "/icons/receiverMarker.png";
-    private static final String DONOR_MARKER = "/icons/deadDonorMarker.png";
-    private static final Integer ZOOM_LEVEL = 5;
-    private static final String FULL_PREFERRED_NAME = "fullPreferredName";
-    private static final Double LAT = -41.0;
-    private static final Double LONG = 172.6362;
-
     private odms.controller.user.OrganMap controller = new odms.controller.user.OrganMap();
     private User currentUser;
-    private Collection<Marker> currentDonorMarkers = new ArrayList<>();
-    private Collection<Marker> currentReceiverMarkers = new ArrayList<>();
+    private Collection<Marker> currentDonorMarkers = new ArrayList<Marker>();
+    private Collection<Marker> currentReceiverMarkers = new ArrayList<Marker>();
     private ObservableList<Profile> donorsList;
+    private ProfileDAO profileDAO = DAOFactory.getProfileDao();
     private ObservableList<Profile> receiversList;
+
+    private final String receiverMarker = "/icons/red_marker.png";
+    private final String donorMarker = "/icons/blue_marker.png";
 
     @FXML
     private GoogleMapView mapView;
     private GoogleMap map;
 
     @FXML
-    private TableView<Profile> donorListView;
+    private TableView donorListView;
     @FXML
-    private TableColumn<Object, Object> donorColumn;
+    private TableColumn donorColumn;
     @FXML
-    private TableView<Profile> receiverListView;
+    private Button showAllDonorsButton;
     @FXML
-    private TableColumn<Object, Object> receiverColumn;
+    private TableView receiverListView;
+    @FXML
+    private TableColumn receiverColumn;
+    @FXML
+    private Button showAllReceiversButton;
 
     private ClinicianProfile parentView;
 
-
-    /**
-     * Sets the current user and parent view.
-     *
-     * @param currentUser the current user.
-     * @param parentView the parent view.
-     */
-    public void initialize(User currentUser, ClinicianProfile parentView) {
-        this.currentUser = currentUser;
-        this.parentView = parentView;
-        controller.setView(this);
-        initListViews();
-    }
-
-    /**
-     * Initializes the map view by adding a listener.
-     *
-     * @param location location.
-     * @param resources resource bundle.
-     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         mapView.addMapInializedListener(this);
     }
 
-    /**
-     * Initializes the map view. Sets the map options.
-     */
     @Override
     public void mapInitialized() {
 
         //Set the initial properties of the map.
         MapOptions mapOptions = new MapOptions();
 
-        mapOptions.center(new LatLong(LAT, LONG)
-)
+        mapOptions.center(new LatLong(-41, 172.6362))
                 .mapType(MapTypeIdEnum.ROADMAP)
                 .overviewMapControl(false)
                 .panControl(false)
@@ -104,25 +77,28 @@ public class OrganMap extends CommonView implements Initializable, MapComponentI
                 .scaleControl(false)
                 .streetViewControl(false)
                 .zoomControl(false)
-                .zoom(ZOOM_LEVEL);
+                .zoom(5);
 
         map = mapView.createMap(mapOptions);
-        showAllDonors();
     }
 
-    /**
-     * Initializes the list views.
-     */
+
+    public void initialize(User currentUser, ClinicianProfile parentView) {
+        this.currentUser = currentUser;
+        this.parentView = parentView;
+        controller.setView(this);
+        initListViews();
+    }
+
     private void initListViews() {
         donorsList = controller.getDeadDonors();
         donorListView.setItems(donorsList);
-        donorColumn.setCellValueFactory(new PropertyValueFactory<>(FULL_PREFERRED_NAME));
+        donorColumn.setCellValueFactory(new PropertyValueFactory<>("fullPreferredName"));
 
         donorListView.setOnMousePressed(event -> {
             if (event.isPrimaryButtonDown() && event.getClickCount() == 2 &&
                     donorListView.getSelectionModel().getSelectedItem() != null) {
-                Profile selectedDonor = donorListView.getSelectionModel()
-                        .getSelectedItem();
+                Profile selectedDonor = (Profile) donorListView.getSelectionModel().getSelectedItem();
                 addDonorMarker(selectedDonor);
                 populateReceivers(selectedDonor);
             }
@@ -131,8 +107,7 @@ public class OrganMap extends CommonView implements Initializable, MapComponentI
         receiverListView.setOnMousePressed(event -> {
             if (event.isPrimaryButtonDown() && event.getClickCount() == 2 &&
                     receiverListView.getSelectionModel().getSelectedItem() != null) {
-                Profile selectedReceiver = receiverListView.getSelectionModel()
-                        .getSelectedItem();
+                Profile selectedReceiver = (Profile) receiverListView.getSelectionModel().getSelectedItem();
                 addReceiverMarker(selectedReceiver);
             }
         });
@@ -140,136 +115,109 @@ public class OrganMap extends CommonView implements Initializable, MapComponentI
 
     /**
      * Populates the receivers table with all the possible receivers for the donor selected.
-     *
      * @param donor the donor that is donating the organ.
      */
     private void populateReceivers(Profile donor) {
         receiversList = controller.getReceivers(donor);
         receiverListView.setItems(receiversList);
-        receiverColumn.setCellValueFactory(new PropertyValueFactory<>(FULL_PREFERRED_NAME));
+        receiverColumn.setCellValueFactory(new PropertyValueFactory<>("fullPreferredName"));
     }
 
-    /**
-     * Adds a single donor marker to the map.
-     *
-     * @param profile profile to add.
-     */
-    private void addDonorMarker(Profile profile) {
-        List<Double> latLng = controller.getProfileLatLong(profile);
+    public void addDonorMarker(Profile profile) {
+        ArrayList<Double> latLng = controller.displayPointOnMap(profile);
         LatLong donorLocation = new LatLong(latLng.get(0), latLng.get(1));
         MarkerOptions markerOptions = new MarkerOptions();
-        formatMarkerImage(DONOR_MARKER, donorLocation, markerOptions);
-
-        Marker marker = new Marker(markerOptions);
-        if (!currentDonorMarkers.isEmpty()) {
-            clearDonorMarkers();
-        }
-        if (!currentReceiverMarkers.isEmpty()) {
-            map.removeMarkers(currentReceiverMarkers);
-            currentReceiverMarkers.clear();
-        }
-        map.addMarker(marker);
-
-        map.addUIEventHandler(marker, UIEventType.click,
-                jsObject -> createNewDonorWindow(profile, parentView, currentUser));
-
-        currentDonorMarkers.add(marker);
-    }
-
-    /**
-     * Adds a receiver marker to the map at the profiles location.
-     *
-     * @param profile The profile to be added to the map.
-     */
-    private void addReceiverMarker(Profile profile) {
-        List<Double> latLng = controller.getProfileLatLong(profile);
-        LatLong donorLocation = new LatLong(latLng.get(0), latLng.get(1));
-        MarkerOptions markerOptions = new MarkerOptions();
-        formatMarkerImage(RECEIVER_MARKER, donorLocation, markerOptions);
-        Marker marker = new Marker(markerOptions);
-
-        if (!currentReceiverMarkers.isEmpty()) {
-            map.removeMarkers(currentReceiverMarkers);
-            currentReceiverMarkers.clear();
-        }
-
-        map.addMarker(marker);
-        map.addUIEventHandler(marker, UIEventType.click,
-                jsObject -> createNewDonorWindow(profile, parentView, currentUser));
-
-        currentReceiverMarkers.add(marker);
-    }
-
-    /**
-     * Shows all the donors on the map.
-     */
-    public void showAllDonors() {
-        map.removeMarkers(currentDonorMarkers);
-        currentDonorMarkers.clear();
-        showAllOnMap(donorsList, DONOR_MARKER);
-    }
-
-    /**
-     * Removes all the donor markers currently on the map.
-     */
-    private void clearDonorMarkers() {
-        map.removeMarkers(currentDonorMarkers);
-        currentDonorMarkers.clear();
-    }
-
-    /**
-     * Adds a marker on the map for each receiver.
-     */
-    public void showAllReceivers() {
-        map.removeMarkers(currentReceiverMarkers);
-        currentReceiverMarkers.clear();
-        showAllOnMap(receiversList, RECEIVER_MARKER);
-
-    }
-
-    /**
-     * Displays a list of profiles on the map.
-     *
-     * @param profileList Profiles to display.
-     * @param mapMarker image to use as marker.
-     */
-    private void showAllOnMap(ObservableList<Profile> profileList, String mapMarker) {
-        for (Profile profile : profileList) {
-            List<Double> latLng = controller.getProfileLatLong(profile);
-            LatLong donorLocation = new LatLong(latLng.get(0), latLng.get(1));
-            MarkerOptions markerOptions = new MarkerOptions();
-            formatMarkerImage(mapMarker, donorLocation, markerOptions);
-            Marker marker = new Marker(markerOptions);
-            currentReceiverMarkers.add(marker);
-
-            map.addUIEventHandler(marker, UIEventType.click,
-                    jsObject -> {
-                        if (mapMarker.equals(DONOR_MARKER)) {
-                            populateReceivers(profile);
-                            showAllReceivers();
-                            clearDonorMarkers();
-                        } else {
-                            createNewDonorWindow(profile, parentView, currentUser);
-                        }
-                    });
-            map.addMarker(marker);
-        }
-    }
-
-    /**
-     * Formats the marker image.
-     *
-     * @param mapMarker The current map marker.
-     * @param donorLocation Donors location as a LatLong.
-     * @param markerOptions The markers options.
-     */
-    private void formatMarkerImage(String mapMarker, LatLong donorLocation,
-            MarkerOptions markerOptions) {
         String markerImage = MarkerImageFactory.createMarkerImage(this.getClass()
-                .getResource(mapMarker).toString(), "png");
+                .getResource("/icons/deadDonorMarker.png").toString(), "png");
         markerImage = markerImage.replace("(", "");
         markerImage = markerImage.replace(")", "");
         markerOptions.position(donorLocation).icon(markerImage);
+
+        Marker marker = new Marker(markerOptions);
+        if(!currentDonorMarkers.isEmpty()){
+            map.removeMarkers(currentDonorMarkers);
+            currentDonorMarkers.clear();
+        }
+        if(!currentReceiverMarkers.isEmpty()){
+            map.removeMarkers(currentReceiverMarkers);
+            currentReceiverMarkers.clear();
+        }
+        map.addMarker(marker);
+        map.addUIEventHandler(marker, UIEventType.click,
+                jsObject -> {
+                    try {
+                        createNewDonorWindow(profileDAO.get(1), parentView, currentUser);
+                    } catch (SQLException e) {
+                        log.error(e.getMessage());
+                    }
+                });
+        currentDonorMarkers.add(marker);
+    }
+
+    public void addReceiverMarker(Profile profile) {
+        ArrayList<Double> latLng = controller.displayPointOnMap(profile);
+        LatLong donorLocation = new LatLong(latLng.get(0), latLng.get(1));
+        MarkerOptions markerOptions = new MarkerOptions();
+        String markerImage = MarkerImageFactory.createMarkerImage(this.getClass()
+                .getResource(receiverMarker).toString(), "png");
+        markerImage = markerImage.replace("(", "");
+        markerImage = markerImage.replace(")", "");
+        markerOptions.position(donorLocation).icon(markerImage);
+        Marker marker = new Marker(markerOptions);
+
+        if (!currentReceiverMarkers.isEmpty()) {
+                    map.removeMarkers(currentReceiverMarkers);
+                    currentReceiverMarkers.clear();
+        }
+
+        map.addMarker(marker);
+        map.addUIEventHandler(marker, UIEventType.click,
+                jsObject -> {
+                    try {
+                        createNewDonorWindow(profileDAO.get(1), parentView, currentUser);
+                    } catch (SQLException e) {
+                        log.error(e.getMessage());
+                    }
+                });
+        currentReceiverMarkers.add(marker);
+    }
+
+    public void showAllDonors() {
+        map.removeMarkers(currentDonorMarkers);
+        currentDonorMarkers.clear();
+        for (Profile profile : donorsList) {
+            ArrayList<Double> latLng = controller.displayPointOnMap(profile);
+            LatLong donorLocation = new LatLong(latLng.get(0), latLng.get(1));
+            MarkerOptions markerOptions = new MarkerOptions();
+            String markerImage = MarkerImageFactory.createMarkerImage(this.getClass()
+                    .getResource("/icons/deadDonorMarker.png").toString(), "png");
+            markerImage = markerImage.replace("(", "");
+            markerImage = markerImage.replace(")", "");
+            markerOptions.position(donorLocation).icon(markerImage);
+            Marker marker = new Marker(markerOptions);
+            currentDonorMarkers.add(marker);
+            map.addMarker(marker);
+        }
+        donorListView.getSelectionModel().clearSelection();
+        receiverListView.rem
+    }
+
+    public void showAllReceivers() {
+        map.removeMarkers(currentReceiverMarkers);
+        currentReceiverMarkers.clear();
+        for(Profile profile : receiversList) {
+            ArrayList<Double> latLng = controller.displayPointOnMap(profile);
+            LatLong donorLocation = new LatLong(latLng.get(0), latLng.get(1));
+            MarkerOptions markerOptions = new MarkerOptions();
+            String markerImage = MarkerImageFactory.createMarkerImage(this.getClass()
+                    .getResource(receiverMarker).toString(), "png");
+            markerImage = markerImage.replace("(", "");
+            markerImage = markerImage.replace(")", "");
+            markerOptions.position(donorLocation).icon(markerImage);
+            Marker marker = new Marker(markerOptions);
+            currentReceiverMarkers.add(marker);
+            map.addMarker(marker);
+        }
     }
 
     public double getMatchesListViewWidth() {
