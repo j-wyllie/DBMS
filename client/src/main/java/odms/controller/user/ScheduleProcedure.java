fd1e1aa253b8843ebaa09105949da8ec3fcfff55
Tim Hamblin <tha110@uclive.ac.nz>
2018-09-15 16:17:53 +1200

    The organ dropdown is now populated -Created a function to get the possible organ donations between two users (assuming they are compatible) and then populates the dropdown with this value #story[1095]

--- client/src/main/java/odms/controller/user/ScheduleProcedure.java
+++ client/src/main/java/odms/controller/user/ScheduleProcedure.java
@@ -1,15 +1,30 @@
 package odms.controller.user;
 
+import odms.commons.model.enums.OrganEnum;
+import odms.commons.model.profile.Profile;
 import odms.controller.CommonController;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * The controller for the scheduling a donation.
  */
 public class ScheduleProcedure extends CommonController {
 
-    private odms.view.user.ScheduleProcedure view;
-
-    public void setView(odms.view.user.ScheduleProcedure view) {
-        this.view = view;
+    /**
+     * Gets the organs that can be donated between two users.
+     * NOTE: This function assumes that the two profiles are compatible.
+     *
+     * @param donor the donor
+     * @param receiver the receiver
+     * @return A list of organs
+     */
+    public List<OrganEnum> getDonatingOrgans(Profile donor, Profile receiver) {
+        Set<OrganEnum> intersection = new HashSet<>(donor.getOrgansDonating());
+        intersection.retainAll(receiver.getOrgansRequired());
+        return new ArrayList<>(intersection);
     }
 }
--- client/src/main/java/odms/view/user/ScheduleProcedure.java
+++ client/src/main/java/odms/view/user/ScheduleProcedure.java
@@ -1,7 +1,11 @@
 package odms.view.user;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import odms.commons.model.enums.OrganEnum;
 import odms.commons.model.profile.Profile;
 import odms.view.CommonView;
 
@@ -10,7 +14,8 @@ import odms.view.CommonView;
  */
 public class ScheduleProcedure extends CommonView {
 
-    private odms.controller.user.ScheduleProcedure controller = new odms.controller.user.ScheduleProcedure();
+    private odms.controller.user.ScheduleProcedure controller;
+    private ObservableList<OrganEnum> organsToDonate = FXCollections.observableArrayList();
 
     private Profile donor;
     private Profile receiver;
@@ -19,6 +24,8 @@ public class ScheduleProcedure extends CommonView {
     private Label donorNameLabel;
     @FXML
     private Label receiverNameLabel;
+    @FXML
+    private ChoiceBox<OrganEnum> organDropdown;
 
     /**
      * Initialize the component and use the parent view to get donor and receiver.
@@ -26,15 +33,29 @@ public class ScheduleProcedure extends CommonView {
      * @param parentView the organ map parent view.
      */
     public void initialize(OrganMap parentView) {
-        controller.setView(this);
+        controller = new odms.controller.user.ScheduleProcedure();
         this.donor = parentView.getCurrentDonor();
         this.receiver = parentView.getCurrentReceiver();
         setLabels();
+        setOrganDropdown();
     }
 
-
+    /**
+     * Sets the name labels.
+     *
+     */
     private void setLabels() {
         donorNameLabel.setText(donor.getFullName());
         receiverNameLabel.setText(receiver.getFullName());
+    }
+
+    /**
+     * Populates the organ dropdown with the possible organs and selects the first one.
+     *
+     */
+    private void setOrganDropdown() {
+        organsToDonate.addAll(controller.getDonatingOrgans(donor, receiver));
+        organDropdown.setItems(organsToDonate);
+        organDropdown.setValue(organsToDonate.get(0));
     }
 }
