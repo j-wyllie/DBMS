75792b99c6706cc25191d7f038d6eba28cf9d841
Tim Hamblin <tha110@uclive.ac.nz>
2018-09-15 16:58:27 +1200

    Created handler for the schedule button -created a method call for this -currently the date is validated -added an error message to the fxml -also changed the way the controller accesses the view #story[1095]

--- client/src/main/java/odms/controller/user/ScheduleProcedure.java
+++ client/src/main/java/odms/controller/user/ScheduleProcedure.java
@@ -4,6 +4,7 @@ import odms.commons.model.enums.OrganEnum;
 import odms.commons.model.profile.Profile;
 import odms.controller.CommonController;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -14,17 +15,45 @@ import java.util.Set;
  */
 public class ScheduleProcedure extends CommonController {
 
+    private odms.view.user.ScheduleProcedure view;
+
+    /**
+     * Sets the view.
+     *
+     * @param view The view.
+     */
+    public void setView(odms.view.user.ScheduleProcedure view) {
+        this.view = view;
+    }
+
     /**
      * Gets the organs that can be donated between two users.
      * NOTE: This function assumes that the two profiles are compatible.
      *
-     * @param donor the donor
-     * @param receiver the receiver
      * @return A list of organs
      */
-    public List<OrganEnum> getDonatingOrgans(Profile donor, Profile receiver) {
+    public List<OrganEnum> getDonatingOrgans() {
+        Profile donor = view.getDonor();
+        Profile receiver = view.getReceiver();
         Set<OrganEnum> intersection = new HashSet<>(donor.getOrgansDonating());
         intersection.retainAll(receiver.getOrgansRequired());
         return new ArrayList<>(intersection);
+    }
+
+    /**
+     *
+     * Schedules the organ donation between the two profiles.
+     * @throws IllegalArgumentException When data is incorrectly entered.
+     */
+    public void scheduleProcedure() {
+        LocalDate date = view.getDatePickerValue();
+
+        if (date == null) {
+            throw new IllegalArgumentException("A valid date must be entered");
+        }
+
+        if (date.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("Date can not be before today");
+        }
     }
 }
--- client/src/main/java/odms/view/user/ScheduleProcedure.java
+++ client/src/main/java/odms/view/user/ScheduleProcedure.java
@@ -4,14 +4,20 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import odms.commons.model.enums.OrganEnum;
 import odms.commons.model.profile.Profile;
 import odms.view.CommonView;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDate;
 
 /**
  * The view for scheduling an organ donation.
  */
+@Slf4j
 public class ScheduleProcedure extends CommonView {
 
     private odms.controller.user.ScheduleProcedure controller;
@@ -26,6 +32,10 @@ public class ScheduleProcedure extends CommonView {
     private Label receiverNameLabel;
     @FXML
     private ChoiceBox<OrganEnum> organDropdown;
+    @FXML
+    private DatePicker dateOfProcedurePicker;
+    @FXML
+    private Label errorLabel;
 
     /**
      * Initialize the component and use the parent view to get donor and receiver.
@@ -34,6 +44,7 @@ public class ScheduleProcedure extends CommonView {
      */
     public void initialize(OrganMap parentView) {
         controller = new odms.controller.user.ScheduleProcedure();
+        controller.setView(this);
         this.donor = parentView.getCurrentDonor();
         this.receiver = parentView.getCurrentReceiver();
         setLabels();
@@ -54,8 +65,38 @@ public class ScheduleProcedure extends CommonView {
      *
      */
     private void setOrganDropdown() {
-        organsToDonate.addAll(controller.getDonatingOrgans(donor, receiver));
+        organsToDonate.addAll(controller.getDonatingOrgans());
         organDropdown.setItems(organsToDonate);
         organDropdown.setValue(organsToDonate.get(0));
+    }
+
+    /**
+     * Schedules the procedure and closes the window.
+     *
+     */
+    @FXML
+    private void scheduleProcedure() {
+        try {
+            controller.scheduleProcedure();
+            Stage stage = (Stage) donorNameLabel.getScene().getWindow();
+            stage.close();
+        } catch (IllegalArgumentException e) {
+            errorLabel.setText(e.getMessage());
+            errorLabel.setVisible(true);
+            log.error(e.getMessage());
+        }
+
+    }
+
+    public Profile getDonor() {
+        return donor;
+    }
+
+    public Profile getReceiver() {
+        return receiver;
+    }
+
+    public LocalDate getDatePickerValue() {
+        return dateOfProcedurePicker.getValue();
     }
 }
