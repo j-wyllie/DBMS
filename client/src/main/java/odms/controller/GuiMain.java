ae0b1a92044f9ef1ea4743fda965cb0e846fe5b1
Matt King <mki58@uclive.ac.nz>
2018-09-19 00:13:54 +1200

    Implement a check to make sure the server can be contacted. If it cannot the user in informed.
    #story[1118]

--- client/src/main/java/odms/controller/GuiMain.java
+++ client/src/main/java/odms/controller/GuiMain.java
@@ -1,6 +1,5 @@
 package odms.controller;
 
-import java.sql.SQLException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,13 +7,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 import odms.commons.model.enums.OrganEnum;
+import odms.commons.model.enums.UserType;
 import odms.commons.model.profile.Profile;
+import odms.commons.model.user.User;
 import odms.controller.database.DAOFactory;
 import odms.controller.user.UserNotFoundException;
-import odms.commons.model.user.User;
-import odms.commons.model.enums.UserType;
 
 import java.io.IOException;
+import java.net.Socket;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
@@ -27,6 +28,8 @@ public class GuiMain extends Application {
     private static final String APP_NAME = "ODMS";
     private static final String ADMIN = "admin";
     private static final String CLINICIAN = "0";
+    private static final Integer PORT = 6969;
+    private static final String DOMAIN = "localhost";
 
     private odms.controller.user.AvailableOrgans controller =
             new odms.controller.user.AvailableOrgans();
@@ -39,6 +42,57 @@ public class GuiMain extends Application {
      */
     @Override
     public void start(Stage primaryStage) throws IOException {
+        boolean serverStatus = checkServer();
+
+        if (serverStatus) {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
+            primaryStage.setScene(new Scene(root));
+            primaryStage.setResizable(false);
+            primaryStage.setTitle(APP_NAME);
+            primaryStage.show();
+
+            checkDefaultProfiles();
+
+            // Thread that runs in the background to check if organs have expired since last launch
+            Thread checkOrgan = new Thread(() -> {
+                try {
+                    List<Map.Entry<Profile, OrganEnum>> availableOrgans = controller
+                            .getAllOrgansAvailable();
+                    for (Map.Entry<Profile, OrganEnum> m : availableOrgans) {
+                        controller.checkOrganExpired(m.getValue(), m.getKey());
+                    }
+                } catch (SQLException e) {
+                    log.error(e.getMessage(), e);
+                }
+            });
+            checkOrgan.setDaemon(true);
+            checkOrgan.start();
+        } else {
+            AlertController.guiPopup("Connection to the server could not be established.\n" +
+                    "Please check your internet connection.\n\n" +
+                    "If that doesn't resolve the issue the Human Farm\n" +
+                    "server may be experiencing issues. Apologies for\n" +
+                    "the inconvenience.");
+        }
+    }
+
+    /**
+     * Simple check to verify if a connection to the odms server can be established.
+     * @return boolean true if server is up, false otherwise
+     */
+    private boolean checkServer() {
+        try (Socket s = new Socket(DOMAIN, PORT)) {
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Checks if default admin and clinician profiles exist in the database,
+     * creates them if they don't.
+     */
+    private void checkDefaultProfiles() {
         try {
             DAOFactory.getUserDao().get(ADMIN);
         } catch (UserNotFoundException e) {
@@ -53,27 +107,6 @@ public class GuiMain extends Application {
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
         }
-
-        // Thread that runs in the background to check if organs have expired since last launch
-        Thread checkOrgan = new Thread(() -> {
-            try {
-                List<Map.Entry<Profile, OrganEnum>> availableOrgans = controller
-                        .getAllOrgansAvailable();
-                for (Map.Entry<Profile, OrganEnum> m : availableOrgans) {
-                    controller.checkOrganExpired(m.getValue(), m.getKey());
-                }
-            } catch (SQLException e) {
-                log.error(e.getMessage(), e);
-            }
-        });
-        checkOrgan.setDaemon(true);
-        checkOrgan.start();
-
-        Parent root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
-        primaryStage.setScene(new Scene(root));
-        primaryStage.setResizable(false);
-        primaryStage.setTitle(APP_NAME);
-        primaryStage.show();
     }
 
     /**
