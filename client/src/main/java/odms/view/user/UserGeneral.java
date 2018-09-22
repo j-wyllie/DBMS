d9b2bffcfff9078c85ab675301ac8dd8a854c882
Alex Miller <ami80@uclive.ac.nz>
2018-09-22 20:26:57 +1200

    Remove retired code from UserGeneral controller. #chore #story[1115]

--- client/src/main/java/odms/view/user/UserGeneral.java
+++ client/src/main/java/odms/view/user/UserGeneral.java
@@ -1,14 +1,7 @@
 package odms.view.user;
 
-import java.io.IOException;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
 import odms.commons.model.user.User;
 import odms.data.DefaultLocale;
 
@@ -23,67 +16,12 @@ public class UserGeneral {
     @FXML
     private Label regionLabel;
 
-    private User currentUser;
-
-    /**
-     * Button handler to undo last action.
-     *
-     * @param event clicking on the undo button.
-     */
-    @FXML
-    private void handleUndoButtonClicked(ActionEvent event) throws IOException {
-        //todo replace with standardised?
-        //undoController.undo(GuiMain.getCurrentDatabase());
-        Parent parent = FXMLLoader.load(getClass().getResource("/view/ClinicianProfile.fxml"));
-        Scene newScene = new Scene(parent);
-        Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        appStage.setScene(newScene);
-        appStage.show();
-    }
-
-    /**
-     * Button handler to redo last undo action.
-     *
-     * @param event clicking on the redo button.
-     */
-    @FXML
-    private void handleRedoButtonClicked(ActionEvent event) throws IOException {
-        //todo replace with standardised?
-        //redoController.redo(GuiMain.getCurrentDatabase());
-        Parent parent = FXMLLoader.load(getClass().getResource("/view/ClinicianProfile.fxml"));
-        Scene newScene = new Scene(parent);
-        Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        appStage.setScene(newScene);
-        appStage.show();
-    }
-
-    /**
-     * Button handler to make fields editable.
-     *
-     * @param event clicking on the edit button.
-     */
-    @FXML
-    private void handleEditButtonClicked(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setLocation(getClass().getResource("/view/ClinicianProfileEdit.fxml"));
-
-        Scene scene = new Scene(fxmlLoader.load());
-        ClinicianEdit v = fxmlLoader.getController();
-        v.initialize(currentUser);
-
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setTitle("Edit profile");
-        stage.setScene(scene);
-        stage.show();
-    }
-
     /**
      * Initializes all of the labels and checks the user type.
      *
      * @param currentUser The current user logged in.
      */
     public void initialize(User currentUser) {
-        this.currentUser = currentUser;
         givenNamesLabel.setText(givenNamesLabel.getText() + (
                         currentUser.getName() != null ? currentUser.getName() : ""));
         staffIdLabel.setText(
