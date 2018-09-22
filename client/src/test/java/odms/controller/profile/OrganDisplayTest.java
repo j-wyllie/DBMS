0078b3720de785c916d04800cc27d3c0426e2636
Matt <mki58@uclive.ac.nz>
2018-09-22 16:07:12 +1200

    Fixed test for getting valid organs to donate. Stubbed the DAO method getExpired.
    #story[1117]

--- client/src/test/java/odms/controller/profile/OrganDisplayTest.java
+++ client/src/test/java/odms/controller/profile/OrganDisplayTest.java
@@ -1,35 +1,26 @@
 package odms.controller.profile;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import odms.commons.model.enums.OrganEnum;
 import odms.commons.model.profile.ExpiredOrgan;
 import odms.commons.model.profile.OrganConflictException;
 import odms.commons.model.profile.Profile;
 import odms.controller.database.organ.HttpOrganDAO;
-import odms.controller.database.organ.OrganDAO;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
-import static org.powermock.configuration.ConfigurationType.PowerMock;
-
-@RunWith(PowerMockRunner.class);
-@PrepareForTest(OrganDAO.class);
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(HttpOrganDAO.class)
 public class OrganDisplayTest {
 
     private odms.view.profile.OrganDisplay view;
@@ -44,23 +35,26 @@ public class OrganDisplayTest {
         profileOneAttr.add("dob=\"17-01-1998\"");
         profileOneAttr.add("nhi=\"123456879\"");
         currentProfile = new Profile(profileOneAttr);
+        controller = new OrganDisplay(view);
         currentProfile.setId(99999);
     }
 
     @Test
     public void getDonatingOrgans() throws OrganConflictException, SQLException {
-        PowerMockito.
+        // Mock makeRequests method, returns ArrayList of Expired Organ objects
+        List<ExpiredOrgan> expired = new ArrayList<>();
+        expired.add(new ExpiredOrgan(OrganEnum.BONE, "", "", null));
+        PowerMockito.stub(PowerMockito.method(HttpOrganDAO.class, "getExpired"))
+                .toReturn(expired);
 
+        // Add donating organs to profile
         currentProfile.addOrganDonating(OrganEnum.LIVER);
         currentProfile.addOrganDonating(OrganEnum.BONE);
         Set<OrganEnum> organs = controller.getDonatingOrgans(currentProfile);
-        for (OrganEnum organ : organs) {
-            System.out.println(organ.getNamePlain());
-        }
+
+        // Bone should not be in the resulting set since it has been manually
+        // expired (provided by mocked method).
         assertTrue(organs.contains(OrganEnum.LIVER));
-        assertTrue(organs.contains(OrganEnum.BONE));
         assertEquals(1, organs.size());
-
-
     }
 }
