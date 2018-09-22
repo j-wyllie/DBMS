052f916b6ed777fb6c61c7a1c327cb1250ec1b53
Joshua <jwy31@uclive.ac.nz>
2018-09-22 14:40:16 +1200

    Changed matching to Miguel's specification. Completely rewrote HLAType changing HLAs to HashMaps and adding secondary Antigen types.
    #story[1109]

--- client/src/main/java/odms/controller/HlaController.java
+++ client/src/main/java/odms/controller/HlaController.java
@@ -1,123 +1,69 @@
 package odms.controller;
 
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 import odms.commons.model.profile.HLAType;
 
 @Slf4j
 public class HlaController {
-    private static final String REGEX_GENE_CLASS_I = "[A-C]";
-    private static final String REGEX_GENE_CLASS_II = "(DP|DQ|DR)";
-    private static final String REGEX_ALLELE = "[0-9]{1,3}";
-    private static final String REGEX_CLASS_I = "(" + REGEX_GENE_CLASS_I + REGEX_ALLELE + ")";
-    private static final String REGEX_CLASS_II = "(" + REGEX_GENE_CLASS_II + REGEX_ALLELE + ")";
-    private static final String REGEX_VALID_ALL = "((" + REGEX_GENE_CLASS_I +
-            "|" + REGEX_GENE_CLASS_II +
-            ")" + REGEX_ALLELE + ")";
+    /**
+     * Returns the best match of the given antigens of the given gene
+     * @param gene the gene to be matched
+     * @param hlaA the first antigen
+     * @param hlaB the second antigen
+     * @return number of matching antigens (0-2)
+     */
+    private static Integer calcMatch(String gene, HLAType hlaA, HLAType hlaB) {
+        Integer xa = hlaA.getGroupX().get(gene);
+        Integer xb = hlaA.getGroupY().get(gene);
+        Integer ya = hlaB.getGroupX().get(gene);
+        Integer yb = hlaB.getGroupY().get(gene);
 
-    private static List<String> hlaTestStrings = Arrays.asList(
-            "A4",
-            "A45",
-            "A690",
-            "B9",
-            "B90",
-            "B901",
-            "C1",
-            "C119",
-            "C16",
-            "DP1",
-            "DP16",
-            "DP130",
-            "DQ3",
-            "DQ12",
-            "DQ923",
-            "DR0",
-            "DR90",
-            "DR990",
-            "DR",
-            "990",
-            "(*&^@$(*&^#$(*#&",
-            "D",
-            "A"
-    );
+        // try matching same groups
+        Integer numMatchingSame = 0;
+        if (xa == xb) {
+            numMatchingSame++;
+        }
+        if (ya == yb) {
+            numMatchingSame++;
+        }
+
+        // try matching cross groups
+        Integer numMatchingCross = 0;
+        if (xa == ya) {
+            numMatchingCross++;
+        }
+        if (ya == xb) {
+            numMatchingCross++;
+        }
+
+        return Math.max(numMatchingSame, numMatchingCross);
+    }
 
     /**
      * Returns with a score of match fit as a percentage.
      *
-     * @param hla1 first HLA to compare
-     * @param hla2 second HLA to compare
+     * @param hlaA first HLA to compare
+     * @param hlaB second HLA to compare
      * @return match fit as a percentage
      */
-    public static int matchScore(HLAType hla1, HLAType hla2) {
-        final float GENE_MATCH_MULTIPLIER = 100f / 12f;
+    public static Integer matchScore(HLAType hlaA, HLAType hlaB) {
+        final float MATCH_MULTIPLIER = 100f / 12f;
         float score = 0;
-        int numMatchingGenes = 0;
+        int numMatchingAnitgens = 0;
 
-        numMatchingGenes += numUniqueMatches(hla1.getG1c1(), hla2.getG1c1());
-        numMatchingGenes += numUniqueMatches(hla1.getG1c2(), hla2.getG1c2());
-        numMatchingGenes += numUniqueMatches(hla1.getG2c1(), hla2.getG2c1());
-        numMatchingGenes += numUniqueMatches(hla1.getG2c2(), hla2.getG2c2());
-
-        score = numMatchingGenes; //* GENE_MATCH_MULTIPLIER;
-        return (int)score;
-    }
-
-    /**
-     * Returns the number of matching genes in a given class and group (set of 3 antigens)
-     * of two different HLA's
-     *
-     * @param hlaA first to compare
-     * @param hlaB second to compare
-     * @return number of matching gene pairs
-     */
-    private static int numUniqueMatches(Set<String> hlaA, Set<String> hlaB) {
-        int numMatchingGenes = 0;
-        Set<String> alreadyMatched = new HashSet<>();
-        for (String antigenA : hlaA) {
-            String geneA = antigenA.replaceAll("[\\d]+", "");
-            System.out.println("gene A  " + geneA);
-            for (String antigenB : hlaB) {
-                String geneB = antigenB.replaceAll("[\\d]+", "");
-                System.out.println("gene B  " + geneB);
-                if (geneA.equals(geneB) && !alreadyMatched.contains(geneA)) {
-                    numMatchingGenes += 1;
-                    alreadyMatched.add(geneA);
-                }
-            }
+        for (String gene : HLAType.getPrimaryGeneList()) {
+            numMatchingAnitgens += calcMatch(gene, hlaA, hlaB);
         }
-        return numMatchingGenes;
+
+        score = numMatchingAnitgens * MATCH_MULTIPLIER;
+        return (int) score;
     }
 
     public static void main (String[] args) {
-
-        // regex testing
-        for (String hla : hlaTestStrings) {
-            if (hla.matches(REGEX_CLASS_I)) {
-                log.info(hla + " passed");
-            } else if (hla.matches(REGEX_CLASS_II)) {
-                log.info(hla + " passed");
-            } else {
-                log.info(hla + " failed");
-            }
-        }
-
-
-        // hla matching testing
-        HLAType hlaA = new HLAType();
-        hlaA.setG1c1(new HashSet<>(Arrays.asList("A123", "B123", "C123")));
-        hlaA.setG1c2(new HashSet<>(Arrays.asList("DP123", "DQ123", "DR123")));
-        hlaA.setG2c1(new HashSet<>(Arrays.asList("A123", "B223", "C123")));
-        hlaA.setG2c2(new HashSet<>(Arrays.asList("DP123", "DQ123", "DR123")));
-
-        HLAType hlaB = new HLAType();
-        hlaB.setG1c1(new HashSet<>(Arrays.asList("B123", "B123", "B123")));
-        hlaB.setG1c2(new HashSet<>(Arrays.asList("DP123", "DQ123", "DR123")));
-        hlaB.setG2c1(new HashSet<>(Arrays.asList("A123", "B123", "C123")));
-        hlaB.setG2c2(new HashSet<>(Arrays.asList("DP123", "DQ123", "DR123")));
-
-        System.out.println(matchScore(hlaA, hlaB));
+        HLAType donor = new HLAType(41, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40);
+        HLAType receiver = new HLAType(40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40);
+        System.out.println("match score " + matchScore(donor, receiver));
     }
 }
--- commons/src/main/java/odms/commons/model/profile/HLAType.java
+++ commons/src/main/java/odms/commons/model/profile/HLAType.java
@@ -1,30 +1,93 @@
 package odms.commons.model.profile;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class HLAType {
-    private Set<String> g1c1 = new HashSet<>();
-    private Set<String> g1c2 = new HashSet<>();
-    private Set<String> g2c1 = new HashSet<>();
-    private Set<String> g2c2 = new HashSet<>();
+    // validation regex
+    private static final String REGEX_GENE_CLASS_I = "[A-C]";
+    private static final String REGEX_GENE_CLASS_II = "(DP|DQ|DR)";
+    private static final String REGEX_ALLELE = "[0-9]{1,3}";
+    private static final String REGEX_CLASS_I = "(" + REGEX_GENE_CLASS_I + REGEX_ALLELE + ")";
+    private static final String REGEX_CLASS_II = "(" + REGEX_GENE_CLASS_II + REGEX_ALLELE + ")";
+    private static final String REGEX_VALID_ALL = "((" + REGEX_GENE_CLASS_I +
+            "|" + REGEX_GENE_CLASS_II +
+            ")" + REGEX_ALLELE + ")";
 
-    public HLAType() {}
+    private static List<String> primaryGeneList = Arrays.asList("A", "B", "C", "DP", "DQ", "DR");
+    private Map<String, Integer> groupX = new HashMap<>();
+    private Map<String, Integer> groupY = new HashMap<>();
+    private Map<String, Integer> secondaryAntigens = new HashMap<>();
 
-    public HLAType(Set<String> g1c1, Set<String> g1c2, Set<String> g2c1, Set<String> g2c2) {
-        this.g1c1 = g1c1;
-        this.g1c2 = g1c2;
-        this.g2c1 = g2c1;
-        this.g2c2 = g2c2;
+    /**
+     * Initialises the keys of the HLA type
+     */
+    public HLAType() {
+        // initialise group X keys
+        groupX.put("A", null);
+        groupX.put("B", null);
+        groupX.put("C", null);
+        groupX.put("DP", null);
+        groupX.put("DQ", null);
+        groupX.put("DR", null);
+
+        // initialise group Y keys
+        groupY.put("A", null);
+        groupY.put("B", null);
+        groupY.put("C", null);
+        groupY.put("DP", null);
+        groupY.put("DQ", null);
+        groupY.put("DR", null);
     }
 
-    public void setG1c1(Set<String> g1c1) { this.g1c1 = g1c1; }
-    public void setG1c2(Set<String> g1c2) { this.g1c2 = g1c2; }
-    public void setG2c1(Set<String> g2c1) { this.g2c1 = g2c1; }
-    public void setG2c2(Set<String> g2c2) { this.g2c2 = g2c2; }
-    public Set<String> getG1c1() { return g1c1; }
-    public Set<String> getG1c2() { return g1c2; }
-    public Set<String> getG2c1() { return g2c1; }
-    public Set<String> getG2c2() { return g2c2; }
+    /**
+     *  Initialises the key-value pairs of the HLA type
+     * @param xa  Group X gene A  Allele
+     * @param xb  Group X gene B  Allele
+     * @param xc  Group X gene C  Allele
+     * @param xdp Group X gene DP Allele
+     * @param xdq Group X gene DQ Allele
+     * @param xdr Group X gene DR Allele
+     * @param ya  Group Y gene A  Allele
+     * @param yb  Group Y gene B  Allele
+     * @param yc  Group Y gene C  Allele
+     * @param ydp Group Y gene DQ Allele
+     * @param ydq Group Y gene DP Allele
+     * @param ydr Group Y gene DR Allele
+     */
+    public HLAType(Integer xa, Integer xb, Integer xc, Integer xdp, Integer xdq, Integer xdr,
+            Integer ya, Integer yb, Integer yc, Integer ydp, Integer ydq, Integer ydr) {
+        // initialise group X
+        groupX.put("A", xa);
+        groupX.put("B", xb);
+        groupX.put("C", xc);
+        groupX.put("DP", xdp);
+        groupX.put("DQ", xdq);
+        groupX.put("DR", xdr);
+
+        // initialise group Y
+        groupY.put("A", ya);
+        groupY.put("B", yb);
+        groupY.put("C", yc);
+        groupY.put("DP", ydp);
+        groupY.put("DQ", ydq);
+        groupY.put("DR", ydr);
+    }
+
+    public static List<String> getPrimaryGeneList() { return primaryGeneList; }
+
+    public Map<String, Integer> getGroupX() { return groupX; }
+    public Map<String, Integer> getGroupY() { return groupY; }
+
+    public void addSecondaryAntigen(String gene, Integer allele) {
+        secondaryAntigens.put(gene, allele);
+    }
+    public Map getSecondaryAntigens() {
+        return secondaryAntigens;
+    }
 }
 
